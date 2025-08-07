f99a73b247b00b67753463d97f8b787639406dc2
cesar augusto castro tello <attrib1986@gmail.com>
2025-08-06 22:27:06 -0500

    fix: agregar import os al settings

--- inspecciones_rutinarias/settings.py
+++ inspecciones_rutinarias/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -120,7 +121,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-import os
+
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static')
 ]
@@ -132,6 +133,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
 
 # Ejecutar migraciones automáticamente en producción (Render)
 if os.environ.get('RENDER'):
