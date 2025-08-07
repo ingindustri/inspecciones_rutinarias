fc332e1e3e730c435aa1690f2468221908e2eebd
cesar augusto castro tello <attrib1986@gmail.com>
2025-08-06 22:48:05 -0500

    Ejecutar migrate automáticamente desde wsgi.py en Render

--- inspecciones_rutinarias/wsgi.py
+++ inspecciones_rutinarias/wsgi.py
@@ -7,16 +7,17 @@ For more information on this file, see
 https://docs.djangoproject.com/en/5.2/howto/deployment/wsgi/
 """
 import os
-import django
 from django.core.wsgi import get_wsgi_application
-from django.core.management import call_command
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'inspecciones_rutinarias.settings')
 
-django.setup()
-
-# Ejecuta migraciones y collectstatic automáticamente
-call_command('migrate')
-call_command('collectstatic', '--noinput')
-
 application = get_wsgi_application()
+
+# Ejecutar migrate automáticamente SOLO si las apps ya están listas
+try:
+    from django.core.management import call_command
+    call_command('migrate', interactive=False)
+except Exception as e:
+    # Evita que Render se bloquee si falla migrate
+    print(f"Error ejecutando migrate: {e}")
+
