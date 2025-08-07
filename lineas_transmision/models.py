41e292387b46520ae1cd3930321595fcc825f7e9
cesar augusto castro tello <attrib1986@gmail.com>
2025-08-07 09:18:40 -0500

    Corrige error de sintaxis en campo CharField

--- lineas_transmision/models.py
+++ lineas_transmision/models.py
@@ -7,19 +7,18 @@ OPCIONES_ESTADO = [
 ]
 
 class InspeccionLinea(models.Model):
-    linea_transmision = models.CharField(max_length=100)
-    terna = models.CharField(max_length=50,default='OK')
+    linea_transmision = models.CharField(max_length=20)
+    terna = models.CharField(max_length=50,default='-')
     fecha = models.DateField()
-    numero_torre = models.CharField(max_length=20, default='OK')
-    pintura = models.CharField(max_length=1, choices=OPCIONES_ESTADO, default='OK')
-    estructuras = models.CharField(max_length=1, choices=OPCIONES_ESTADO, default='OK')
-    fundaciones = models.CharField(max_length=1, choices=OPCIONES_ESTADO, default='OK')
-    pernos = models.CharField(max_length=1, choices=OPCIONES_ESTADO, default='OK')
-    conductor = models.CharField(max_length=1, choices=OPCIONES_ESTADO, default='OK')
-    aisladores = models.CharField(max_length=1, choices=OPCIONES_ESTADO, default='OK')
-    herrajes = models.CharField(max_length=1, choices=OPCIONES_ESTADO, default='OK')
+    numero_torre = models.CharField(max_length=20, default='-')
+    pintura = models.CharField(max_length=20, choices=OPCIONES_ESTADO, default='-')
+    estructuras = models.CharField(max_length=20, choices=OPCIONES_ESTADO, default='-')
+    fundaciones = models.CharField(max_length=20, choices=OPCIONES_ESTADO, default='-')
+    pernos = models.CharField(max_length=20, choices=OPCIONES_ESTADO, default='-')
+    conductor = models.CharField(max_length=20, choices=OPCIONES_ESTADO, default='-')
+    aisladores = models.CharField(max_length=20, choices=OPCIONES_ESTADO, default='-')
+    herrajes = models.CharField(max_length=20, choices=OPCIONES_ESTADO, default='-')
     observaciones = models.TextField(blank=True)
-    estructuras = models.TextField(blank=True, null=True)
 
     def __str__(self):
         return f"{self.linea_transmision} - Torre {self.numero_torre}"
