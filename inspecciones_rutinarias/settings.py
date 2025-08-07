67f81771e6ae521179c84815d524cb9307737e86
cesar augusto castro tello <attrib1986@gmail.com>
2025-08-07 00:11:53 -0500

    Corrijo configuración de DATABASES para Render

--- inspecciones_rutinarias/settings.py
+++ inspecciones_rutinarias/settings.py
@@ -74,13 +74,21 @@ WSGI_APPLICATION = 'inspecciones_rutinarias.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
-DATABASES = {
-    'default': dj_database_url.config(
-        default='postgres://usuario:contraseña@host:puerto/dbname',
-        conn_max_age=600,
-        ssl_require=True
-    )
-}
+if os.getenv("RENDER"):
+    DATABASES = {
+        'default': dj_database_url.config(
+            conn_max_age=600,
+            ssl_require=True
+        )
+    }
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+        }
+    }
+
 
 # Password validation
 # https://docs.djangoproject.com/en/5.2/ref/settings/#auth-password-validators
