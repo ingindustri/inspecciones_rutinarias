19fee40cdde856e2b5c2e59816ded673f5308439
cesar augusto castro tello <attrib1986@gmail.com>
2025-08-06 22:34:54 -0500

    Mover migraciones automáticas del settings.py al wsgi.py

--- inspecciones_rutinarias/wsgi.py
+++ inspecciones_rutinarias/wsgi.py
@@ -6,16 +6,17 @@ It exposes the WSGI callable as a module-level variable named ``application``.
 For more information on this file, see
 https://docs.djangoproject.com/en/5.2/howto/deployment/wsgi/
 """
-
 import os
-
+import django
 from django.core.wsgi import get_wsgi_application
+from django.core.management import call_command
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'inspecciones_rutinarias.settings')
 
-application = get_wsgi_application()
+django.setup()
 
-if os.environ.get('RENDER'):
-    from django.core.management import call_command
-    call_command('migrate')
-    call_command('collectstatic', interactive=False)
+# Ejecuta migraciones y collectstatic automáticamente
+call_command('migrate')
+call_command('collectstatic', '--noinput')
+
+application = get_wsgi_application()
