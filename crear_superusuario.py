bea043468e4eb75621c063616d0f72558306b1cc
cesar augusto castro tello <attrib1986@gmail.com>
2025-08-06 23:34:48 -0500

    Agregar script para migraciones y superusuario automático en Render

--- crear_superusuario.py
+++ crear_superusuario.py
@@ -2,13 +2,9 @@ from django.contrib.auth import get_user_model
 
 User = get_user_model()
 
-# Cambia los datos aquí si deseas usar otros
-USERNAME = "admin"
-EMAIL = "admin@user.com"
-PASSWORD = "admin1234"
-
-if not User.objects.filter(username=USERNAME).exists():
-    User.objects.create_superuser(USERNAME, EMAIL, PASSWORD)
-    print("✅ Superusuario creado.")
+if not User.objects.filter(username='admin').exists():
+    User.objects.create_superuser('admin', 'admin@example.com', 'admin123')
+    print("Superusuario creado")
 else:
-    print("ℹ️ El superusuario ya existe.")
+    print("El superusuario ya existe.")
+
